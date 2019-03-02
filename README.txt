96c92aa7e47ce64ae9f440eb46ba0379ca6d468f
guennator <corentin.leguennec@gmail.com>
2019-03-02 01:26:50 +0100

    link issu solved using regex, bug : cannot write mode p as jpeg

---
+++ README.txt
@@ -0,0 +1,3 @@
+Hi,
+
+this is an ongoing project, the idea being to download scans of your manga.
--- mangadownloader.py
+++ mangadownloader.py
@@ -5,7 +5,6 @@ from PIL import Image
 import io
 import os
 import re
-import time
 
 
 url2 =  "https://www.scan-vf.co/uploads/manga/one_piece/chapters/chapitre-1/01.jpg"
@@ -17,54 +16,66 @@ base_url =  "https://www.scan-vf.co/uploads/manga/one_piece/chapters"
 #byteImg = Image.open(f)
 #byteImg.save('test.jpg', 'JPEG')
 
-def test():
-    fwcURL = "https://www.scan-vf.co/one_piece/chapitre-2" #URL to read
-    req = urllib.request.Request(fwcURL,  headers={'User-Agent' : "Magic Browser"})
-    mylines = urllib.request.urlopen(req).readlines()
-    print("Found URLs:")
-    match = re.findall(r'https://ww[w4].scan-vf.com?/uploads/manga/([^\'" >]+)', str(mylines))
-    print(match)
-    #for line in mylines:
-        #print(line)
-        #time.sleep(1) #Pause execution for a bit
-        #links = re.findall('"((http|ftp)s?://.*?)"', str(line))  
-        #match = re.findall(r'href=[\'"]?([^\'" >]+)', str(line))
-        #print (match)
-    for i in match:
-        print(i)
+def fetch_url_data(url):
+    #print("fetching image url ")
+    try :
+        req = urllib.request.Request(url,  headers={'User-Agent' : "Magic Browser"})
+        return urllib.request.urlopen(req)
+    except:
+        print("error 502 reloading page")
+        return fetch_url_data(url)
 
-def load_chapter(manga_name, chap_nbr):
-    fwcURL = "https://www.scan-vf.co/" + manga_name + "/chapitre-" + str(chap_nbr) #URL to read
-    req = urllib.request.Request(fwcURL,  headers={'User-Agent' : "Magic Browser"})
-    mylines = urllib.request.urlopen(req).readlines()
+def fetch_url(url):
+    try :
+        req = urllib.request.Request(url,  headers={'User-Agent' : "Magic Browser"})
+        return urllib.request.urlopen(req).readlines()
+    except:
+        print("error 502 reloading chapter")
+        return fetch_url_data(url)
+
+def load_chapter(manga_name, chap_url, chap_nbr):
+    fwcURL = "https://www.scan-vf.co/" + manga_name + chap_url #URL to read
+    mylines = fetch_url(fwcURL)
     print("Found URLs:")
     match = re.findall(r'data-src=\\[\']?\s([^\'" >]+)', str(mylines))
-    for page in range(len(match)):
-        req = urllib.request.Request(match[page],  headers={'User-Agent' : "Magic Browser"})
-        con = urllib.request.urlopen(req)
+    match_len = len(match)
+    for page in range(match_len):
+        con = fetch_url_data(match[page])
         f = io.BytesIO(con.read())
         byteImg = Image.open(f)
-        byteImg.save(os.path.expanduser("~/Documents/" + manga_name + "/chapter-" + str(chap_nbr) + "/" + '%02d' % (page + 1)  + ".jpg"), 'JPEG')
-        print("page number " + '%02d' % (page + 1))
+        end = match[page][-3:]
+        if end == "png" :
+            byteImg.save(os.path.expanduser("~/Documents/MangaDownloader/" + manga_name + "/chapter-" + str(chap_nbr) + "/" + '%02d' % (page + 1)  + ".png"), 'PNG')
+            print("page number " + '%02d' % (page + 1) + ".png")
+        else:
+            byteImg.save(os.path.expanduser("~/Documents/MangaDownloader/" + manga_name + "/chapter-" + str(chap_nbr) + "/" + '%02d' % (page + 1)  + ".jpg"), "JPEG")
+            print("page number " + '%02d' % (page + 1) + ".jpg")
     print()
 
-
+def find_chapter(manga_name):
+    fwcURL = "https://www.scan-vf.co/" + manga_name #URL to read
+    mylines = fetch_url(fwcURL)
+    match = re.findall(r'href=[\'"]?' + fwcURL + '([^\'" >]+)', str(mylines))
+    return match
+    
 
 def folder_manager(manga_name, chapter):
-    url = "https://www.scan-vf.co/uploads/manga/" + manga_name + "/chapters"
-    done = 1
-    chap_nbr = 1
-    if not(os.path.isdir(os.path.expanduser("~/Documents/" + manga_name))):
-        os.mkdir(os.path.expanduser("~/Documents/" + manga_name))
-    while done <= chapter:
-        if not(os.path.isdir(os.path.expanduser("~/Documents/" + manga_name + "/chapter-" + str(done)))):
-            print("Downloading Chapter " + str(done) + "\n")
-            os.mkdir(os.path.expanduser("~/Documents/" + manga_name + "/chapter-" + str(done)))
-            load_chapter(manga_name, done)
-        else :
-            print("Chapter already downloaded" + str(done) + "\n")
-        done += 1
-    print("finished chapters \n")
+    if not(os.path.isdir(os.path.expanduser("~/Documents/MangaDownloader/" + manga_name))):
+        os.mkdir(os.path.expanduser("~/Documents/MangaDownloader/" + manga_name))
+    chapters_links = find_chapter(manga_name)
+    print("chapters Urls: " + str(len(chapters_links)) + " founded")
+    chapter_rank = 1
+    if chapter == 0:
+        chapter = len(chapters_links)
+    for i in range (len(chapters_links) - 1,len(chapters_links) - chapter - 1 ,-1):
+        if not(os.path.isdir(os.path.expanduser("~/Documents/MangaDownloader/" + manga_name + "/chapter-" + str(chapter_rank)))):
+            print("downloading chapter - " + '%02d' % chapter_rank + " / " + str(chapter - chapter_rank) +" left to download\n")
+            os.mkdir(os.path.expanduser("~/Documents/MangaDownloader/" + manga_name + "/chapter-" + str(chapter_rank)))
+            load_chapter(manga_name, chapters_links[i], chapter_rank)
+        else:
+            print("chapter - " + '%02d' % (chapter_rank) + " already downloaded / " + str(chapter - i - 1) +"left to download\n")
+        chapter_rank += 1
+        
 
 
 def manga_dl():
@@ -77,6 +88,7 @@ def manga_dl():
     except:
         print("nope")
 
-folder_manager("one_piece", 250)
+folder_manager("one_piece",0)
+
 
 
